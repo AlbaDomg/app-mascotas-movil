c84a5e1ef95d1669c4c58bf77daba631e1b56291
Alba Domínguez García <albadege94@gmail.com>
2026-05-18 02:51:22 +0200

    Eliminando plugin de react que sobraba

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Si no tienes plugins de PWA todavía, déjalo así de simple para que funcione el despliegue
-  server: {
-    port: 5173
-  }
+  // Configuración mínima para que funcione en Vercel
 })
